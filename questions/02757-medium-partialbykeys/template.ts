67c0c653ac5504f3f1343bcd52e08817e4ab533b
幻夜つかさ <69581083+dan-kuroto@users.noreply.github.com>
2025-07-02 21:59:05 +0800

    解题 2693, 2757

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,9 @@
-type PartialByKeys<T, K> = any
+namespace t02757 {
+  export type All<T> = { [K in keyof T]: T[K] }
+}
+
+type PartialByKeys<T, K extends keyof T = any> = t02757.All<{
+  [key in keyof T as key extends K ? key : never]?: T[key]
+} & {
+  [key in keyof T as key extends K ? never : key]: T[key]
+}>
