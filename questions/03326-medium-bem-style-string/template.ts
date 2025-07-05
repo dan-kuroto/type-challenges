51049938cc8e3adf9fe8824ed05593182ae9538c
幻夜つかさ <69581083+dan-kuroto@users.noreply.github.com>
2025-07-05 12:45:52 +0800

    解题 3326

--- questions/03326-medium-bem-style-string/template.ts
+++ questions/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,7 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type BEM<B extends string, E extends string[], M extends string[]> = [
+  B,
+  E[number] extends never ? '' : `__${E[number]}`,
+  M[number] extends never ? '' : `--${M[number]}`,
+] extends [infer A extends string, infer B extends string, infer C extends string]
+  ? `${A}${B}${C}`
+  : never
